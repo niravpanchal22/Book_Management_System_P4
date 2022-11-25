e178c4500ebedf56924a144a61391ef32c952851
niravpanchal22 <niravpanchal226@gmail.com>
2022-11-25 20:47:57 +0530

    added data.json files & postman requests

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const {users} = require('./data/usrs.json');
+
 const app = express();
 
 const PORT = 8081;
@@ -10,6 +12,55 @@ app.get("/", (req, res) => {
   res.status(500).json({
     message: "Server is up and running",
   });
+});
+
+app.get("/users", (req, res) => {
+    res.status(200).json({
+      success: true,
+      data: users,
+    });
+  });
+
+app.get('/users/:id', (req, res) => {
+    const  { id } = req.params;
+    const user = users.find((each ) => each.id === id);
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "user not found",
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        data: user,
+    }); 
+});
+
+app.post("/users", (req, res) => {
+    const { id, name, surname, email, subscriptionType, subscriptionDate } =
+    req.body;
+
+    const user = users.find((each) => each.id === id);
+
+    if (user) {
+       return res.status(404).json({
+        success: false,
+        message: "user exist with this id",
+       });
+    }
+    
+    user.push({
+        id,
+        name,
+        surname,
+        email,
+        subscriptionType,
+        subscriptionDate,
+    });
+    return res.status(201).json({
+      success: true,
+      data: users,
+    });
 });
 
 app.get("*", (req, res) => {
